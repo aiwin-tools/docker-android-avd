498ef7254ca680332948028a2784539c44dcbae9
Jesus Martin Alonso <jesus.martin@aiwin.es>
2019-05-08 09:38:29 +0200

    Put "immersive_mode_confirmations" as "confirmed" to avoid showing dialog "Viewing full screen"

--- prepare-emulator.sh
+++ prepare-emulator.sh
@@ -6,3 +6,4 @@ adb shell settings put global transition_animation_scale 0 &
 adb shell settings put global animator_duration_scale 0 &
 adb shell input keyevent 82 &
 adb shell settings put secure show_ime_with_hard_keyboard 0 &
+adb shell settings put secure immersive_mode_confirmations confirmed
